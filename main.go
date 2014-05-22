0699ad7a2a86ff4d59d8991a0ebc402ea4ff9ff0
gocode <mail2mohanram@gmail.com>
2014-05-23 00:30:55 +0530

    created handler to send file list in json format

--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
-	_ "fmt"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
+
+type FileList struct {
+	Name  string
+	IsDir bool
+}
+
+var workspaces []string
+
+func init() {
+	workspaces = make([]string, 10)
+	workspaces[0] = "D:\\music"
+}
+
+func handler(rw http.ResponseWriter, req *http.Request) {
+	os.Chdir(workspaces[0])
+	files, err := ioutil.ReadDir(workspaces[0])
+	if err != nil {
+
+	}
+	var fileList = make([]FileList, len(files))
+	for i, file := range files {
+		fileList[i].Name = file.Name()
+		fileList[i].IsDir = file.IsDir()
+	}
+
+	f, err := json.Marshal(fileList)
+	if err != nil {
+
+	}
+	fmt.Fprint(rw, f)
+}
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", handler)
 	http.ListenAndServe("localhost:9091", nil)
 }
--- static/js/mediaplayer.js
+++ static/js/mediaplayer.js
@@ -1,32 +1,18 @@
 var app=angular.module('mediaplayer',[]);
 
-function MediaPlayerCtrl($scope) {
-	$scope.files = [
-		{"name":"song1", "size":"5 MB", "duration":"05:00:00", "src":"path/file", "type":"audio/mpeg"},
-		{"name":"song2", "size":"6 MB", "duration":"05:00:00", "src":"path/file", "type":"audio/ogg"},
-		{"name":"song3", "size":"7 MB", "duration":"05:00:00", "src":"path/file", "selected":"info"},
-		{"name":"song4", "size":"8 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song5", "size":"9 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song1", "size":"5 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song2", "size":"6 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song3", "size":"7 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song4", "size":"8 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song5", "size":"9 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song1", "size":"5 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song2", "size":"6 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song3", "size":"7 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song4", "size":"8 MB", "duration":"05:00:00", "src":"path/file"},
-		{"name":"song5", "size":"9 MB", "duration":"05:00:00", "src":"path/file"},
-	]
+function MediaPlayerCtrl($scope, $http) {
 
-	$scope.srcfile = "";
+	$http.get("/").
+    	success(function(data) {
+    		$scope.files = data;
+    	});
 
-	$scope.play = function(file) {
-		console.log("file selected is " + file.src);
-		$scope.srcfile = file.src;
-		for (var i = $scope.files.length - 1; i >= 0; i--) {
-			$scope.files[i].selected = "";
-		};
-		file.selected = "bg-primary";
-	};
+	// $scope.play = function(file) {
+	// 	console.log("file selected is " + file.src);
+	// 	$scope.srcfile = file.src;
+	// 	for (var i = $scope.files.length - 1; i >= 0; i--) {
+	// 		$scope.files[i].selected = "";
+	// 	};
+	// 	file.selected = "bg-primary";
+	// };
 };
