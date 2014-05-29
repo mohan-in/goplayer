09191d18e07983272e8bae8b50b8e09c9bc859ad
gocode <mail2mohanram@gmail.com>
2014-05-30 00:30:49 +0530

    got the player working

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
+	"path/filepath"
 )
 
 type FileList struct {
-	Name  string
-	IsDir bool
+	Name    string
+	IsDir   bool
+	AbsPath string
 }
 
 var workspaces []string
@@ -20,27 +21,40 @@ func init() {
 	workspaces[0] = "D:\\music"
 }
 
-func handler(rw http.ResponseWriter, req *http.Request) {
-	os.Chdir(workspaces[0])
-	files, err := ioutil.ReadDir(workspaces[0])
-	if err != nil {
+func handler(rw http.ResponseWriter, r *http.Request) {
 
+	path := ""
+	if path = r.URL.Query().Get("path"); path == "" {
+		path = workspaces[0]
 	}
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	var fileList = make([]FileList, len(files))
 	for i, file := range files {
 		fileList[i].Name = file.Name()
 		fileList[i].IsDir = file.IsDir()
+		fileList[i].AbsPath = filepath.Join(path, file.Name())
 	}
 
-	f, err := json.Marshal(fileList)
-	if err != nil {
-
+	enc := json.NewEncoder(rw)
+	if err := enc.Encode(fileList); err != nil {
+		fmt.Println(err)
 	}
-	fmt.Fprint(rw, f)
+}
+
+func fileServer(rw http.ResponseWriter, r *http.Request) {
+	http.ServeFile(rw, r, r.URL.Query().Get("file"))
 }
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+	http.HandleFunc("/mediafile/", fileServer)
 	http.ListenAndServe("localhost:9091", nil)
 }
--- static/js/mediaplayer.js
+++ static/js/mediaplayer.js
@@ -2,17 +2,21 @@ var app=angular.module('mediaplayer',[]);
 
 function MediaPlayerCtrl($scope, $http) {
 
-	$http.get("/").
-    	success(function(data) {
+	$http.get("/").success(function(data) {
     		$scope.files = data;
-    	});
+    });
 
-	// $scope.play = function(file) {
-	// 	console.log("file selected is " + file.src);
-	// 	$scope.srcfile = file.src;
-	// 	for (var i = $scope.files.length - 1; i >= 0; i--) {
-	// 		$scope.files[i].selected = "";
-	// 	};
-	// 	file.selected = "bg-primary";
-	// };
+	$scope.srcfile = "";
+
+	$scope.play = function(file) {
+		console.log("file selected is " + file.AbsPath);
+		if(file.IsDir === true) {
+			$http.get("/?path="+file.AbsPath).success(function(data) {
+				$scope.files = data;
+			});
+		} else {
+			$scope.srcfile = "/mediafile/?file=" + file.AbsPath;
+		}
+		
+	};
 };
