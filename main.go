a79867b17b348aaf83dc3068199d9658be89d7b0
gocode <mail2mohanram@gmail.com>
2016-01-27 23:03:30 +0530

    refactored

--- main.go
+++ main.go
@@ -49,5 +49,5 @@ func main() {
 	http.HandleFunc("/dir", directoryHandler)
 	http.HandleFunc("/static/", staticFilesHandler)
 	http.HandleFunc("/media/", mediaFileHandler)
-	http.ListenAndServe("localhost:9090", nil)
+	http.ListenAndServe(":9090", nil)
 }
